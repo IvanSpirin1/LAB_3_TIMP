1e0f3844477d2ffb85ed533a39e005b9a91992fd
IvanSpirin1 <145967592+IvanSpirin1@users.noreply.github.com>
2023-12-19 13:48:06 +0300

    Update main.cpp

--- lab3/lab3_2/main.cpp
+++ lab3/lab3_2/main.cpp
@@ -1,108 +1,58 @@
 #include <UnitTest++/UnitTest++.h>
-#include "shifr.h"
-#include <codecvt>
-#include <locale>
-#include <string>
 #include <iostream>
-SUITE(ShifrTests)
-{
-    TEST(ValidKeyTest) {
-        Shifr shifr(5);
-        int result = shifr.getValidKey(5);
-        int expected = 5;
-        if (result != expected) {
-            std::cout << "Expected: " << expected << " but got: " << result << std::endl;
-        }
-        CHECK_EQUAL(expected, result);
+#include <string>
+#include <UnitTest++/TestReporterStdout.h>
+#include "shifr.h"
+auto RunSuite (const char* SuiteName)
+{ 
+    UnitTest::TestReporterStdout reporter;
+    UnitTest::TestRunner runner(reporter);
+        return runner.RunTestsIf(UnitTest::Test::GetTestList(),
+                                SuiteName,
+                                UnitTest::True(),0);
+}
+std::string t;
+std::string text;
+int key;
+shifr cipher(key,t);
+SUITE(KeyTest){
+    TEST(key_1){
+    CHECK_THROW(cipher.getValidKeyText(key=10, t="abc"), cipher_error);
     }
-TEST(InvalidKeyTest) {
-    Shifr shifr(5);
-    try {
-        shifr.getValidKey(-1);
-        CHECK(false);
-    } catch (const cipher_error& e) {
-        CHECK(true);
-    }
-
-    try {
-        shifr.getValidKey(0);
-        CHECK(false);
-    } catch (const cipher_error& e) {
-        CHECK(true);
-    }
-
-    try {
-        shifr.getValidKey(2);
-        CHECK(false);
-    } catch (const cipher_error& e) {
-        CHECK(true);
+    TEST(key_2){
+        CHECK_THROW(cipher.getValidKey(key=-3) , cipher_error);
     }
 }
-TEST(EncryptTest) {
-    Shifr shifr(3);
-    std::wstring result = shifr.encrypt(L"Пример текста для шифрования");
-    std::wstring expected = L"ИРЕТД ФВИ РЕТС ЯИОН ПМ КАЛШРАЯ";
 
-    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-    std::string result_str = converter.to_bytes(result);
-    std::string expected_str = converter.to_bytes(expected);
-
-    CHECK_EQUAL(expected_str, result_str);
+SUITE(encrypt){
+    TEST(t_1){
+        t = "abcdefghijklmnopqrstuxyz"; 
+        CHECK_EQUAL(shifr(3,t).encrypt(t), "qiarjbskctldumexnfyogzph");
+    }
+    TEST(t_2){
+        t = "abc21defghi43jklmnopq34rstu18xyz"; 
+        CHECK_EQUAL(shifr(3,t).encrypt(cipher.getValidOpenText(t)), "qiarjbskctldumexnfyogzph");
+    }
+    TEST(t_3){
+        t = "238273983"; 
+        CHECK_THROW(shifr(3,t).encrypt(cipher.getValidOpenText(t)), cipher_error);
+    }    
 }
-TEST(DecryptTest) {
-    Shifr shifr(3);
-    std::wstring encrypted = shifr.encrypt(L"Пример текста");
-    std::wstring result = shifr.decrypt(encrypted);
-    std::wstring expected = L"ПРИМЕР ТЕКСТА";
-
-    // Конвертация в std::string для сравнения с CHECK_EQUAL
-    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-    std::string result_str = converter.to_bytes(result);
-    std::string expected_str = converter.to_bytes(expected);
-    CHECK_EQUAL(expected_str, result_str);
-}
-
-TEST(ValidOpenTextTest) {
-    Shifr shifr(3);
-    std::wstring result = shifr.getValidOpenText(L"пример текста");
-    std::wstring expected = L"ПРИМЕР ТЕКСТА";
-
-    // Конвертация в std::string для сравнения с CHECK_EQUAL
-    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-    std::string result_str = converter.to_bytes(result);
-    std::string expected_str = converter.to_bytes(expected);
-
-    CHECK_EQUAL(expected_str, result_str);
+SUITE(decrypt){
+    TEST(t_1){
+        t = "abcdefghijklmnopqrstuxyz"; 
+        CHECK_EQUAL(shifr(3,t).decrypt(t), "cfiloruzbehknqtyadgjmpsx");
+    }
+    TEST(t_2){
+        t = "abc21defghi43jklmnopq34rstu18xyz"; 
+        CHECK_EQUAL(shifr(3,t).decrypt(cipher.getValidOpenText(t)), "cfiloruzbehknqtyadgjmpsx");
+    }  
 }
 
-TEST(ValidCipherTextTest) {
-    Shifr shifr(3);
-    std::wstring result = shifr.getValidCipherText(L"ШИФРОВАННЫЙ ТЕКСТ");
-    std::wstring expected = L"ШИФРОВАННЫЙ ТЕКСТ";
-
-    // Конвертация в std::string для сравнения с CHECK_EQUAL
-    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-    std::string result_str = converter.to_bytes(result);
-    std::string expected_str = converter.to_bytes(expected);
-
-    CHECK_EQUAL(expected_str, result_str);
-}
-
-TEST(InvalidCipherTextTest) {
-    Shifr shifr(3);
-    CHECK_THROW(shifr.getValidCipherText(L"Шифр с цифрами: 123456"), cipher_error);
-}
-
-TEST(KeyLong) {
-    Shifr shifr(5);
-    std::wstring openText = L"Текст";
-    CHECK_THROW(shifr.cipher_error)
-}
-
-}
-
-int main()
-{
-    std::locale::global(std::locale("ru_RU.UTF-8"));
-    return UnitTest::RunAllTests();
+int main() {
+    RunSuite("KeyTest");
+    RunSuite("encrypt");
+    RunSuite("decrypt");
+    return 0;
+    
 }
