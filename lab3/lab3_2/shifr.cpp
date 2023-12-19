8b1090ddcb201cf235dfb314ea620f454756b2ad
IvanSpirin1 <145967592+IvanSpirin1@users.noreply.github.com>
2023-12-19 13:48:36 +0300

    Update shifr.cpp

--- lab3/lab3_2/shifr.cpp
+++ lab3/lab3_2/shifr.cpp
@@ -1,107 +1,70 @@
 #include "shifr.h"
-#include <iostream>
-#include <locale>
-#include <algorithm>
 
-Shifr::Shifr(int columns) : key(columns) {
-    if (key <= 0) {
-        throw cipher_error("Invalid key");
-    }
-    if (key >= open_text(size)) {
-	throw cipher_error("KeyLong");
-    }
-		
-}
-
-std::wstring Shifr::encrypt(const std::wstring& open_text) {
-    std::wstring tmp = getValidOpenText(open_text);
-
-    // Преобразование всех символов в верхний регистр
-    std::transform(tmp.begin(), tmp.end(), tmp.begin(), [](wchar_t c) {
-        return std::towupper(c);
-    });
-
-    int length = tmp.length();
-    int rows = (length + key - 1) / key;
-
-    std::wstring ciphertext;
-
-    for (int i = key - 1; i >= 0; --i) {
-        for (int j = 0; j < rows; ++j) {
-            int index = j * key + i;
-            if (index < length) {
-                ciphertext += tmp[index];
-            } else {
-                ciphertext += ' ';
-            }
-        }
-    }
-
-    return ciphertext;
-}
-
-
-std::wstring Shifr::decrypt(const std::wstring& cipher_text) {
-    std::wstring tmp = getValidCipherText(cipher_text);
-
-    int length = tmp.length();
-	int rows = (length + key - 1) / key;
-
-	std::wstring text;
-
-	for (int i = 0; i < rows; ++i) {
-		for (int j = key - 1; j >= 0; --j) {
-			int index = i + j * rows;
-			if (index < length) {
-				text += tmp[index];
-			}
-            else{
-                text+=' ';
-            }
-		}
-	}
-
-	return text;
-}
-
-int Shifr::getValidKey(int key) {
-    if (key <= 0  || iswalpha(key)) {
-        throw cipher_error("Invalid key");
+int shifr::getValidKey(int &key){
+    if (key < 2){
+        throw cipher_error("Negative key");
     }
     return key;
 }
-
-
-std::wstring Shifr::getValidOpenText(const std::wstring &s) {
-    std::wstring tmp;
-    for (size_t i = 0; i < s.size(); i++) {
-        wchar_t c = s[i];
-        if ((c >= L'А' && c <= L'Я') || (c >= L'а' && c <= L'я') || c == L'ё' || c == L'Ё' || c==L' ') {
-            tmp.push_back(std::towupper(c));
+std::string shifr::getValidOpenText(std::string &s)
+{
+    std::string tmp;
+    for(auto c:s){
+        if (isalpha(c))
+        {
+           if (isupper(c))
+                tmp.push_back(c);
+            else
+                tmp.push_back(c);
         }
+        
     }
-    if (tmp.empty()) {
-        throw cipher_error("Invalid open text");
-    }
+        if (tmp.empty())
+        {
+            throw cipher_error("Empty open text");
+        }
+        
     return tmp;
 }
-
-
-std::wstring Shifr::getValidCipherText(const std::wstring &s) {
-    std::wstring tmp;
-    for (auto c : s) {
-        if (iswupper(c)) {
-            tmp.push_back(c);
-        }
-        else if(c==L' '){
-            tmp.push_back(L' ');
-        }
-		else{
-			throw cipher_error("Invalid cipher text");
-		}
+int shifr::getValidKeyText(int key, std::string &text){
+    if(key > int(text.length())){
+        throw cipher_error("The key is bigger than the text");
     }
-
-    if (tmp.empty())
-        throw cipher_error("Empty cipher text");
-    return tmp;
+    return 0;
+}
+std::string shifr::encrypt(const std::string& user_str) 
+{
+    std::string new_str = user_str;
+    int length, line_count, position, i; 
+    length = user_str.size(); 
+    line_count = (length - 1) / newkey + 1; 
+    i = 0;
+    for (int colum_number = newkey; colum_number > 0; colum_number--) { 
+        for (int line_number = 0; line_number < line_count; line_number++) { 
+            position = newkey*line_number+colum_number; 
+            if (position-1<length){
+            new_str[position-1] = user_str[i];
+            i++;
+            }
+        }
+    }
+    return new_str;
+}
+std::string shifr::decrypt(const std::string& user_str) 
+{
+    std::string new_str = user_str;
+    int length, line_count, position, i; 
+    length = user_str.size(); 
+    line_count = (length - 1) / newkey + 1; 
+    i = 0;
+    for (int colum_number = newkey; colum_number > 0; colum_number--) { 
+        for (int line_number = 0; line_number < line_count; line_number++) { 
+            position = colum_number+newkey*line_number;
+            if (position-1<length){
+            new_str[i] = user_str[position-1];
+            i++;
+            }
+        }
+    }
+    return new_str;
 }
